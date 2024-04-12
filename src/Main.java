121fc85ec95004cfbca5a827cc878bc17fe3b77b
GHDB ASHEN <ghdbashen@gmail.com>
2024-04-12 12:10:37 +0530

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+class Calculator extends JFrame {
+    JLabel l1, l2, resultLabel, output;
+    JTextField number1TextField, number2TextField;
+    JButton divideButton, additionButton, subtractButton, MultiButton, clearButton;
+
+}
+    public class Main {
+        public static void main(String[] args) {
+            SwingUtilities.invokeLater(() -> {
+                Calculator calculator = new Calculator();
+                calculator.setLocationRelativeTo(null); // Center the frame on the screen
+                calculator.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                calculator.setVisible(true);
+            });
+        }
+    }
